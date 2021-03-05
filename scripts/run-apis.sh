0a3a2417bcdd96c187792c2bff305a9bba4abd0a
schemabuoi <charyszczakg@gmail.com>
2021-03-05 11:36:49 +0100

    Update run-apis.sh

--- scripts/run-apis.sh
+++ scripts/run-apis.sh
@@ -1,4 +1,9 @@
-cd ../SearchEngine.API
 osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
-osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine.API && dotnet watch run --urls=http://localhost:5002/" in window 1'
+osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/API/SearchEngine.API && dotnet watch run --urls=http://localhost:5002/" in window 1'
+sleep 1
+
+osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
+osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/API/SearchEngine.API && dotnet watch run --urls=http://localhost:5004/" in window 1'
+
+cd ../SearchEngine/LoadBalancer/LoadBalancer
 dotnet watch run --urls=http://localhost:5000/
