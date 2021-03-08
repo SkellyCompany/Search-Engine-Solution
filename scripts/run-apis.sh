ed604cc122c5dc596a29cb94af4533244eda1946
schemabuoi <charyszczakg@gmail.com>
2021-03-08 12:39:09 +0100

    Update scripts - Add LoggerAPI

--- scripts/run-apis.sh
+++ scripts/run-apis.sh
@@ -4,6 +4,11 @@ sleep 1
 
 osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
 osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/API/SearchEngine.API && dotnet watch run --urls=http://localhost:5004/" in window 1'
+sleep 1
+
+osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
+osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/LoggerAPI && dotnet watch run --urls=http://localhost:5006/" in window 1'
+sleep 1
 
 cd ../SearchEngine/LoadBalancer/LoadBalancer
 dotnet watch run --urls=http://localhost:5000/
--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -8,20 +8,26 @@ cd LoadBalancer
 git checkout development
 cd ..
 
-### Pull API
+### Pull Crawler
 git clone git@github.com:Skelly-Co/Search-Engine-Crawler.git Crawler
 cd Crawler
 git checkout development
 cd ..
 
-### Pull iOS App
+### Pull Web App
 git clone git@github.com:Skelly-Co/Search-Engine-Web.git Web
 cd Web
 git checkout development
 cd ..
 
-### Pull Web App
+### Pull API
 git clone git@github.com:Skelly-Co/Search-Engine-API.git API
 cd API
+git checkout development
+cd ..
+
+### Pull LoggerAPI
+git clone git@github.com:Skelly-Co/Search-Engine-LoggerAPI.git LoggerAPI
+cd LoggerAPI
 git checkout development
 cd ..
