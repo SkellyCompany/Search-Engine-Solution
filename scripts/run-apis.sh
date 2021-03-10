84ab420c3c98b32a8ae2104f2d8f1a1a14c84ef0
Greg Charyszczak <charyszczakg@gmail.com>
2021-03-10 14:48:00 +0100

    Update run-apis script

--- scripts/run-apis.sh
+++ scripts/run-apis.sh
@@ -1,14 +1,24 @@
+firstHost=$(cat ../SearchEngine/LoadBalancer/appsettings.json  | jq -r '.LoadBalancerSettings.FirstHost')
+hostsPoolSize=$(cat ../SearchEngine/LoadBalancer/appsettings.json  | jq -r '.LoadBalancerSettings.HostsPoolSize')
+hostIncrementationValue=$(cat ../SearchEngine/LoadBalancer/appsettings.json  | jq -r '.LoadBalancerSettings.HostIncrementationValue')
+
+hostNumber=$(echo $firstHost | cut -d':' -f 3)
+hostUrl=$(echo $firstHost | cut -d':' -f 1-2)
+for (( i=0; i<=$hostsPoolSize-1; i++ ))
+do
+apiHost="$hostUrl:$hostNumber"
+
+echo "$apiHost"
 osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
-osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/API/SearchEngine.API && dotnet watch run --urls=http://localhost:5002/" in window 1'
-sleep 1
+osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/API && dotnet watch run --urls='"$apiHost"'" in window 1'
+
+hostNumber=$((hostNumber + hostIncrementationValue))
+sleep 0.5
+done
 
 osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
-osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/API/SearchEngine.API && dotnet watch run --urls=http://localhost:5004/" in window 1'
-sleep 1
-
-osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
-osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/LoggerAPI && dotnet watch run --urls=http://localhost:5006/" in window 1'
-sleep 1
+osascript -e 'tell app "Terminal" to do script "cd ../SearchEngine/LoggerAPI && dotnet watch run --urls=http://localhost:5002/" in window 1'
+sleep 0.5
 
 cd ../SearchEngine/LoadBalancer
 dotnet watch run --urls=http://localhost:5000/
